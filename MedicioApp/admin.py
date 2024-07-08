cf3a70097282c41c2915fd2f083db09884513564
Clarence-14 <thisisclarence1@gmail.com>
2024-07-08 10:43:22 +0300

    commit

--- MedicioApp/admin.py
+++ MedicioApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from MedicioApp.models import Product, Branch, Contact
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(Branch)
+admin.site.register(Contact)
--- MedicioApp/urls.py
+++ MedicioApp/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('inner/', views.inner, name='inner'),
+    path('about/', views.about, name='about'),
+    path('doctors/', views.doctors, name='doctors'),
+    path('departments/', views.departments, name='departments'),
+    path('contact/', views.contact, name='contact'),
 
 ]
